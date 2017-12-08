c78e2f28da8e5e2cbe41e12a168c250c9af02def
Brian Rago <236713@pioimcl13e909bd.aaps.internal>
2017-12-08 11:42:31 -0500

    Fixed Bits initialization Added bitwise operations

--- src/datatypes/Bits.java
+++ src/datatypes/Bits.java
@@ -6,41 +6,49 @@ import java.util.*;
 public class Bits {
 	
 	private List<Bit> bits = new ArrayList<Bit>();
+	public boolean isBigEndian = false; //TODO Implement proper endianness
 	
-	public Bits () {
+	public Bits() {
 		bits = new ArrayList<Bit>();
 	}
 	
-	public Bits (int length) {
-		bits = new ArrayList<Bit>();
-		this.fillBits();
+	public Bits(int length) {
+		bits = new ArrayList<Bit>(length);
+		while(bits.size() < length) {
+			bits.add(new Bit());
+		}
 	}
 	
-	public Bits (Bit[] bitArray) {
+	public Bits(Bit[] bitArray) {
 		bits = Arrays.asList(bitArray);
 	}
 	
-	public Bits (List<Bit> bitList) {
+	public Bits(List<Bit> bitList) {
 		bits = new ArrayList<Bit>(bitList);
 	}
 	
-	public Bits (Bits bitsToCopy) {
+	public Bits(Bits bitsToCopy) {
 		bits = new ArrayList<Bit>(bitsToCopy.bits);
 	}
+	
+	public Bits(boolean[] bitArray) {
+		//TODO
+	}
+	
 	
 	public Bits clone() {
 		return new Bits(this);
 	}
 	
-	public Bit getBit (int index) {
+	public Bit getBit(int index) {
 		return bits.get(index);
 	}
 	
-	public void setBit (int index, Bit newBit) {
+	public void setBit(int index, Bit newBit) {
 		bits.set(index, newBit);
 	}
 	
-	public void setBit (int index) {
+	public void setBit(int index) {
 		bits.set(index, new Bit());
 	}
 	
@@ -54,31 +62,66 @@ public class Bits {
 		return null; //later
 	}*/
 	
-	public Bits binaryBitwise (Bits a, BitOperations.BinaryOperation operation) {
-		Bits out = (this.length() > a.length()? this : a).clone();
+	public Bits binaryBitwise(Bits a, BitOperations.BinaryOperation operation) {
+		Bits longer = this.length() > a.length()? this : a; //I know I could just clone and call it out, but this seems cleaner.
+		Bits shorter = this.length() < a.length()? this : a;
+		Bits out = longer.clone(); //See above
 		
-		int smallLength = Math.min(this.length(), a.length());
-				
-		for (int i = 0; i < smallLength; i++) {
-			
+		for (int i = 0; i < shorter.length(); i++) {
+			out.setBit(i, BitOperations.operate(out.getBit(i), shorter.getBit(i), operation));
 		}
 		
-		return null;
+		return out;
 	}
 	
-	public Bits unaryBitwise (BitOperations.UnaryOperation operation) {
-		return null;
+	public Bits unaryBitwise(BitOperations.UnaryOperation operation) {
+		Bits out = this.clone();
+		
+		for (int i = 0; i < out.length(); i++) {
+			out.setBit(i, BitOperations.operate(out.getBit(i), operation));
+		}
+		
+		return out;
 	}
 	
-	public void fillBits () {
+	public void fillBits() {
 		for (int i = 0; i < bits.size(); i++) {
 			bits.set(i, new Bit());
 		}
 	}
 	
-	public void fillBits (Bit a) {
+	public void fillBits(Bit a) {
 		for (int i = 0; i < bits.size(); i++) {
 			bits.set(i, a.clone());
 		}
+	}
+	
+	public String toString() {
+		String out = "";
+		
+		for(int i = 0; i < this.length() - 1; i++) {
+			out += this.getBit(i).toString() + ", ";
+		}
+		
+		out += this.getBit(this.length() - 1).toString();
+		return out;
+	}
+	
+	public int toInt() {
+		int out = 0;
+		
+		for(int i = 0; i < this.length(); i++) {
+			out += Math.pow(this.getBit(i).getState()? 2 : 0, this.length() - i);
+		}
+		
+		return out;
+	}
+	
+	public static void main(String[] args) {
+		Bits sevenBitByte = new Bits(7);
+		sevenBitByte.setBit(2, new Bit(true));
+		System.out.println(sevenBitByte.length());
+		System.out.println(sevenBitByte.toString());
+		System.out.println(sevenBitByte.toInt());
 	}
 }
